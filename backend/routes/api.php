46414edd934904034a3e6492e78c492ab5767fea
Adam B <adambouadi566@gmail.com>
2026-05-07 18:02:47 +0100

    feat:  la creation

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CandidatureController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+Route::get('/test', function () {
+    return response()->json(['message' => 'API is working!']);
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/candidatures', [CandidatureController::class, 'store']);
+    
+});
