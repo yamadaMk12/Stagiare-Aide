2c689a273f256412b3a52c2107378181504b127c
Adam B <adambouadi566@gmail.com>
2026-05-07 19:59:29 +0100

    Ajout de la fonctionnalité de suppression de candidature

---
+++ backend/app/Http/Controllers/DeleteController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Candidature;
+use Illuminate\Http\Request;
+
+class DeleteController extends Controller
+{
+    public function destroy(Request $request)
+    {
+        $request->validate([
+            'candidature_id' => 'required|exists:candidatures,id',
+        ]);
+
+        $candidature = Candidature::find($request->candidature_id);
+
+        if ($candidature->candidat_id !== $request->user()->id) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Action non autorisée.'
+            ], 403);
+        }
+        $candidature->delete();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Candidature supprimée avec succès !',
+        ], 200);
+    }
+}
