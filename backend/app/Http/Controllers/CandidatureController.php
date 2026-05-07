35e7d3353c167e254cc7df08f79c5364ab4e08fa
Adam B <adambouadi566@gmail.com>
2026-05-07 22:09:59 +0100

    delete candidature

--- backend/app/Http/Controllers/CandidatureController.php
+++ backend/app/Http/Controllers/CandidatureController.php
@@ -1,1 +1,30 @@
-git
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Candidature;
+use Illuminate\Http\Request;
+
+class CandidatureController extends Controller
+{
+    public function destroy(Request $request)
+    {
+        $request->validate([
+            'candidature_id' => 'required|exists:candidatures,id',
+        ]);
+
+        $candidature = Candidature::find($request->candidature_id);
+
+        if ($candidature->candidat_id !== $request->user()->id) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Action non autorisée.'
+            ], 403);
+        }
+        $candidature->delete();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Candidature supprimée avec succès !',
+        ], 200);
+    }
+}
