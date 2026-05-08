61aec84c0ea3ba7f75a73acb095e68e9ad896df5
yamadaMk12 <kite.kun555@gmail.com>
2026-05-08 18:11:42 +0100

    feat: add authentication and profile update routes to api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -10,5 +10,6 @@ Route::post('login', [AuthController::class, 'login']);
 Route::post('register' , [AuthController::class , 'register']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/profile', [ProfilController::class, 'update']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('profile', [ProfilController::class, 'update']);
 });
