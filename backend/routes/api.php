4a6cd69927a02edd1016bb6d2be6d49c814761d9
yamadaMk12 <kite.kun555@gmail.com>
2026-05-08 10:36:42 +0100

    feat: add API routes for profile competences and competence retrieval

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -7,11 +7,7 @@ use App\Http\Controllers\ProfilController;
 use App\Http\Controllers\CompetenceController;
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
 
-    Route::post('/profile', [ProfilController::class, 'update']);
     Route::post('/profile/competences', [ProfilController::class, 'updateCompetences']);
     Route::get('/competences', [CompetenceController::class, 'index']);
 });
