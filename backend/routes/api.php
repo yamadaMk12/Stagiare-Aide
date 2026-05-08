0ab48947f56b2768d54eeff652bdc003f86e39ce
mohamed_01 <med2006jbari@gmail.com>
2026-05-08 15:33:00 +0100

    feat: Login api with sanctum

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/login', [AuthController::class, 'login']);
